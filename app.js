94a9b0e62ac57ca2c8aaa7dfd953312ec7c195ff
Ravish4532 <ravishbarnwal123@example.com>
2025-07-10 15:38:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+function register(detail) {
+    detail.preventDefault(); // Prevent the default form submission behavior
+
+    const name = document.getElementById("nid").value;
+    const contacts = document.getElementById("contacts").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    console.log("Name:", name);
+    console.log("Contact Number:", contacts);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    fetch("https://a5pd6qeg8j.execute-api.ap-south-1.amazonaws.com/dev/send", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nn: name,
+            nc: contacts,
+            ne: email,
+            nm: message
+        })
+    })
+    .then(response => response.json())  // Parse JSON response
+    .then(data => {
+        alert(data.body.message)
+        // Optionally, handle/display the response here
+    })
+    .catch(error => {
+        
+        alert("something went wrong")// Log error
+        // Optionally, handle error here
+    });
+}
